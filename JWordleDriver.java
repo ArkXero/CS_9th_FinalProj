07407f1f61bfb419fb6986181e9126e138d8ba71
Ronit Singh <102002190+ArkXero@users.noreply.github.com>
2025-05-06 20:42:02 -0400

    i have no idea what this commit is

--- JWordleDriver.java
+++ JWordleDriver.java
@@ -1,7 +1,9 @@
 public class JWordleDriver {    
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new WordleGUI());
-        WordleGUI gui = new WordleGUI(); //debug print setup
-        System.out.println("Target word (for testing): " + gui.getGame().getTargetWord()); // debug print
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            // Create and set up the window.
+            WordleGUI gui = new WordleGUI(); //debug print setup
+        System.out.println("Target word (for testing): " + gui.getGame().getTargetWord()); //debug print
+        });
     };
 }
